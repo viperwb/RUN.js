7a6342712c9eedcf79ad5a659d630364585b7c53
JonathanBing <52873238+viperwb@users.noreply.github.com>
2022-11-18 13:25:58 +0800

    Update shouldIRun.js

--- shouldIRun.js
+++ shouldIRun.js
@@ -1,1 +1,7 @@
-
+function checkLanguage (val) {
+  if (val === 'English') return true
+}
+function checkEnglishScore (l,s,r,w) {
+  // 雅思听说读写成绩
+  if (l >= 8 && s >= 8 && r >= 8 && w >= 8 ) return true
+}
